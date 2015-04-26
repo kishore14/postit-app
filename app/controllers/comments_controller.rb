c001a88096a13aad94940281a6d39409f0f0efc2
kishore14 <kishore.rambatla@gmail.com>
2015-04-26 13:36:56 +0000

    Postit with authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
+  before_action :require_user
+  
   def create
     @post = Post.find(params[:post_id])
     #@comment = Comment.new(params.require(:comment).permit(:body))
     #@comment.post = @post
     #Above two can be condesed into below line
     @comment = @post.comments.build(params.require(:comment).permit(:body))
-    @comment.creator = User.first
+    @comment.creator = current_user
     
     if @comment.save
       flash[:notice] = 'Your comment was added.'
@@ -14,4 +16,16 @@ class CommentsController < ApplicationController
       render 'posts/show'
     end
   end
+  
+  def vote
+    @comment = Comment.find(params[:id])
+    vote = Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
+    if vote.valid?
+      flash[:notice] = "Your vote is counted."
+    else
+      flash[:error] = "You can only vote on a comment once."
+    end
+    redirect_to :back
+  end
+  
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :vote]
+  before_action :require_user, except: [:show, :index]
   
   def index
-    @posts = Post.all
+    @posts = Post.all.sort_by{|x| x.total_votes}.reverse
   end
   
   def show
@@ -15,7 +16,7 @@ class PostsController < ApplicationController
   
   def create
     @post = Post.new(post_params)
-    @post.creator = User.first
+    @post.creator = current_user
     if @post.save
       flash[:notice] = "Your post was creted"
       redirect_to posts_path
@@ -24,8 +25,7 @@ class PostsController < ApplicationController
     end
   end
   
-  def edit
-  end
+  def edit; end
   
   def update
     if @post.update(post_params)
@@ -34,6 +34,16 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+  
+  def vote
+    vote = Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
+    if vote.valid?
+      flash[:notice] = "Your vote was counted."  
+    else
+      flash[:error] = "You can only vote once on a post."
+    end
+    redirect_to :back
   end
   
   private
